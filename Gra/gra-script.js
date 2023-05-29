d0c1ee321839e2f94e67ba311b65d881c435b6e8
KapePL <113850207+ToKapePL@users.noreply.github.com>
2023-05-29 20:36:31 +0200

    Update gra-script.js

--- Gra/gra-script.js
+++ Gra/gra-script.js
@@ -11,7 +11,7 @@ let cenaZdrapki = 50; // cena zdrapki
 let zdrapkaZdrapana = false; // flaga, czy zdrapka została zdrapana
 
 kwadrat.addEventListener('click', function() {
-  if (szansaNa2x > 0) {
+  if (szansaNa2x > 1) {
     let losowaLiczba = Math.random(); // losowanie liczby od 0 do 1
 
     if (losowaLiczba < szansaNa2x) {
