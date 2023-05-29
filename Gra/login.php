c6eb0a3942781b4b6c78c08227249a81ebef27af
KapePL <113850207+ToKapePL@users.noreply.github.com>
2023-05-29 16:22:37 +0200

    Create login.php

---
+++ Gra/login.php
@@ -0,0 +1,21 @@
+<?php
+// Pobranie przesłanych danych logowania
+$data = json_decode(file_get_contents("php://input"), true);
+$username = $data["username"];
+$password = $data["password"];
+
+// Sprawdzenie poprawności danych logowania
+// Można tutaj użyć np. bazy danych lub plików do przechowywania danych użytkowników
+$storedUsername = "przykladowy_uzytkownik";
+$storedPassword = "tajnehaslo";
+
+if ($username === $storedUsername && $password === $storedPassword) {
+  // Wysłanie odpowiedzi JSON o sukcesie logowania
+  $response = ["success" => true];
+  echo json_encode($response);
+} else {
+  // Wysłanie odpowiedzi JSON o niepowodzeniu logowania
+  $response = ["success" => false];
+  echo json_encode($response);
+}
+?>
