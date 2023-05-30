ae40b35137ac7b02b4f01f9fadc706231e13f2d4
KapePL <113850207+ToKapePL@users.noreply.github.com>
2023-05-30 11:25:57 +0200

    Update gra-script.js

--- Gra/gra-script.js
+++ Gra/gra-script.js
@@ -4,8 +4,8 @@ let sklep = document.getElementById('sklep');
 let kupButton = document.getElementsByClassName('kup-button');
 let liczbaPunktow = 0;
 let szansaNa2x = 0; 
-let cena2x = 100;
-let Niebieski = 300;
+let cena2x = 10;
+
 
 kwadrat.addEventListener('click', function() {
   let losowaLiczba = Math.random();
@@ -24,15 +24,6 @@ function kupPrzedmiot(0) {
     liczbaPunktow -= cena2x;
     szansaNa2x = 0.15;
     punkty.textContent = 'Punkty: ' + liczbaPunktow;
-    kupButton[index].style.display = 'none'; 
-  } else {
-    alert('Nie masz wystarczającej liczby punktów!');
-  }
-}
-function kupPrzedmiot(1) {
-  if (liczbaPunktow >= Niebieski ) {
-    liczbaPunktow -= Niebieski;
-    document.getElementById('kwadrat1');
     kupButton[index].style.display = 'none'; 
   } else {
     alert('Nie masz wystarczającej liczby punktów!');
