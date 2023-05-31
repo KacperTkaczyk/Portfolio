30abe52ab0e63737897bdf88270d8c7ffb237b3c
KapePL <113850207+ToKapePL@users.noreply.github.com>
2023-05-31 19:43:28 +0200

    Update gra-script.js

--- Gra/gra-script.js
+++ Gra/gra-script.js
@@ -19,34 +19,13 @@ kwadrat.addEventListener('click', function() {
 });
 
 function kupPrzedmiot(index) {
-  if (index === 1 && liczbaPunktow >= 10) {
-    liczbaPunktow -= 10;
+  if (index === 1 && liczbaPunktow >= 300) {
+    liczbaPunktow -= 300;
     szansaNa2x = 0.15;
     punkty.textContent = 'Punkty: ' + liczbaPunktow;
     kupButton[index].style.display = 'none';
-    kwadrat.style.display = 'none';
-    let kwadrat1 = document.createElement('div');
-    kwadrat1.id = 'kwadrat1';
-    kwadrat1.style.width = '200px';
-    kwadrat1.style.height = '200px';
-    kwadrat1.style.backgroundColor = 'blue';
-    kwadrat1.style.position = 'absolute';
-    kwadrat1.style.top = '50%';
-    kwadrat1.style.left = '50%';
-    kwadrat1.style.transform = 'translate(-50%, -50%)';
-    kwadrat1.style.cursor = 'pointer';
-    document.body.appendChild(kwadrat1);
-    kwadrat1.addEventListener('click', function() {
-      let losowaLiczba = Math.random();
-    
-      if (losowaLiczba < szansaNa2x) {
-        liczbaPunktow += 2;
-      } else {
-        liczbaPunktow++;
-      }
-    
-      punkty.textContent = 'Punkty: ' + liczbaPunktow;
-    });
+    kwadrat.style.backgroundColor = 'blue';
+    kwadrat.removeEventListener('click', handleClick);
   } else {
     alert('Nie masz wystarczającej liczby punktów lub nieprawidłowy indeks przedmiotu!');
   }
